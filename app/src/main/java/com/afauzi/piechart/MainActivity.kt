a85ed2cc4368ab640c0b639eddc7b3b7fa8928e0
Akhmad Fauzi <akhmadfauzi0101@gmail.com>
2023-04-02 19:44:29 +0700

    Initial commit

---
+++ app/src/main/java/com/afauzi/piechart/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.afauzi.piechart
+
+import android.graphics.Color
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.afauzi.piechart.databinding.ActivityMainBinding
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.utils.MPPointF
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var pieChart: PieChart
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        pieChart = binding.pieChart
+        pieChart.setUsePercentValues(true)
+        pieChart.description.isEnabled = false
+        pieChart.setExtraOffsets(5f, 10f, 5f, 5f)
+
+        pieChart.dragDecelerationFrictionCoef = 0.95f
+
+        pieChart.isDrawHoleEnabled = true
+        pieChart.setHoleColor(Color.WHITE)
+
+        pieChart.setTransparentCircleColor(Color.WHITE)
+        pieChart.setTransparentCircleAlpha(110)
+
+        pieChart.holeRadius = 58f
+        pieChart.transparentCircleRadius = 61f
+
+        pieChart.setDrawCenterText(true)
+
+        pieChart.rotationAngle = 0f
+
+        pieChart.isRotationEnabled = true
+        pieChart.isHighlightPerTapEnabled = true
+
+        pieChart.animateY(1400, Easing.EaseInOutQuad)
+
+        pieChart.legend.isEnabled = false
+        pieChart.setEntryLabelColor(Color.WHITE)
+        pieChart.setEntryLabelTextSize(12f)
+
+        val entries: ArrayList<PieEntry> = arrayListOf()
+        entries.add(PieEntry(70f))
+        entries.add(PieEntry(20f))
+        entries.add(PieEntry(10f))
+
+        val dataSet = PieDataSet(entries, "Warehouse count")
+
+        dataSet.setDrawIcons(false)
+
+        dataSet.sliceSpace = 3f
+        dataSet.iconsOffset = MPPointF(0f, 40f)
+        dataSet.selectionShift = 5f
+
+        val colors: ArrayList<Int> = arrayListOf()
+        colors.add(resources.getColor(R.color.purple_200))
+        colors.add(resources.getColor(R.color.purple_500))
+        colors.add(resources.getColor(R.color.purple_700))
+
+        dataSet.colors = colors
+
+        val data = PieData(dataSet)
+        data.setValueFormatter(PercentFormatter())
+        data.setValueTextSize(15f)
+        data.setValueTypeface(Typeface.DEFAULT_BOLD)
+        data.setValueTextColor(Color.WHITE)
+
+        pieChart.data = data
+
+        pieChart.highlightValues(null)
+
+        pieChart.invalidate()
+
+    }
+}
